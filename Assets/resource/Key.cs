2857225b3498f844bdff7188080528f41fee0c89
kaipippi <87025607+kaipippi@users.noreply.github.com>
2021-08-12 13:04:53 +0900

    input fix

---
+++ Assets/resource/Key.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+
+    public void pointordown()
+    {
+        if (!this.GetComponent<AudioSource>().isPlaying)
+        {
+            this.GetComponent<AudioSource>().Play();
+        }
+    }
+
+    public void click()
+    {
+        this.GetComponent<AudioSource>().Stop();
+    }
+
+}
